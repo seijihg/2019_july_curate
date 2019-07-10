1459b2de6c5feca402f9d1a09c2938cb6b621e7e
Louise Norris <louisenorris73@yahoo.co.uk>
2019-07-10 17:42:22 +0100

    CRUD for like and comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,16 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new", as: "login"
   post "login", to: "sessions#create"
   get "logout", to: "sessions#destroy", as: "logout"
+  post "like", to: "likes#create_index"
 
   resources :sessions, only: [:new, :create, :destroy]
-  resources :exhibitions
   resources :users, only: [:new, :create, :show, :edit, :update]
-  resources :comments
   resources :comment_likes
-  resources :likes
+  resources :likes, only: [:create]
+
+  resources :exhibitions do
+    resources :comments, except: [:index, :new, :show], shallow: true
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
